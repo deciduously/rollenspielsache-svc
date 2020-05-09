9bbaf8c01c612833ecb8e7579a7cfc606f1c063b
Ben Lovy <ben@deciduously.com>
2020-05-09 17:20:56 -0400

    0.1.1 (#2)
    * FIx README links
    
    * Add CORS headers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 //! rollenspielsache-svc is a backend web service for working with [librollenspielsache](https://docs.rs/librollenspielsache) types
 
-use actix_web::{App, HttpServer};
+use actix_service::Service;
+use actix_web::{
+    http::{header::*, HeaderValue},
+    App, HttpServer,
+};
 use listenfd::ListenFd;
 use log::info;
 
@@ -21,7 +25,26 @@ async fn main() -> std::io::Result<()> {
     // Set up autoreloader
     let mut listenfd = ListenFd::from_env();
     // Define server
-    let mut server = HttpServer::new(|| App::new().service(index).service(pong).service(roll));
+    let mut server = HttpServer::new(|| {
+        App::new()
+            .wrap_fn(|req, srv| {
+                // CORS headers
+                let fut = srv.call(req);
+                async {
+                    let mut res = fut.await?;
+                    res.headers_mut().insert(
+                        ACCESS_CONTROL_ALLOW_ORIGIN,
+                        HeaderValue::from_static("https://rollenspielsache.deciduously.com"),
+                    );
+                    res.headers_mut()
+                        .insert(VARY, HeaderValue::from_static("Origin"));
+                    Ok(res)
+                }
+            })
+            .service(index)
+            .service(pong)
+            .service(roll)
+    });
     // Catch reload
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)?
